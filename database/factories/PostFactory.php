b446aba1cc9a17f176ed1a203c5a51efb5dda83e
Asad Shahid <asadshahid45892@gmail.com>
2022-08-05 23:06:06 +0500

    First commit

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->title,
+            'description'=> $this->faker->paragraph,
+            'featured_image' => '',
+            'category_id' => Category::inRandomOrder()->take(1)->first()->id,
+            'user_id' => User::inRandomOrder()->take(1)->first()->id,
+        ];
+    }
+}
